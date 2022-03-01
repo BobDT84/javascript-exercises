fe71b81085eb1e7981349dd3ecca1f693372ad04
Bob <bobdt84@gmail.com>
2022-02-28 20:08:57 -0600

    Completed exercise 11 and 12.  Exercise 13 is a work in progress.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let ages = [];
+    let thisYear = new Date();
+    thisYear = thisYear.getFullYear();
+    for(let i=0; i<people.length; i++){
+        if(isNaN(people[i].yearOfDeath)){
+            ages[i] = thisYear-people[i].yearOfBirth
+        } else {
+            ages[i] = people[i].yearOfDeath - people[i].yearOfBirth
+        }
+    }
+    let index = 0;
+    indexOfOldest = ages.indexOf(Math.max(...ages));
+    return people[indexOfOldest];
 };
 
 // Do not edit below this line
--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,7 @@
-const caesar = function() {
-
+const caesar = function(message,shift) {
+    let codedMessage = '';
+    let alphabetRE = [a-zA-Z];
+    let lower = message.toLowerCase();
 };
 
 // Do not edit below this line
