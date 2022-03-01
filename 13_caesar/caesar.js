7a1f33d14b25201791ccffab8dc32c5a063974e7
Bob <bobdt84@gmail.com>
2022-02-28 20:56:12 -0600

    Exercise 13 still a work in progress.  Attempting to wrap the shift back to a letter in the alphabet when the unicode shift extends too far

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,8 +1,50 @@
 const caesar = function(message,shift) {
-    let codedMessage = '';
-    let alphabetRE = [a-zA-Z];
-    let lower = message.toLowerCase();
+    let messageArray = message.split('');
+    let endOfArray = messageArray.length;
+    let char = '';
+
+    for(let i=0; i<endOfArray; i++){
+        char = messageArray[i];
+        if(isInAlphabet(char)){
+            messageArray[i] = shiftCharacter(char,shift);
+        }
+    }
+    return messageArray.join('');
 };
 
+function isInAlphabet(char){
+    return char == char.match(/[a-zA-Z]/);
+};
+
+function shiftCharacter(char,shift){
+    let charUnicode = char.codePointAt(0);
+    let shiftedChar = '';
+    if(isAlphabetUnicode(shiftedUnicode)){
+        shiftedChar = String.fromCharCode(shiftedUnicode);
+    } else {
+        
+    }
+
+    return shiftedChar;
+};
+
+function isAlphabetUnicode(unicode){
+    if(unicode >= 97 && unicode <= 122){
+        return true;
+    } else if (unicode >= 65 && unicode <= 90){
+        return true;
+    } else {
+        return false;
+    }
+};
+
+function isLowerAlphabetUnicode(unicode){
+
+};
+
+function unicodeShift(unicode,shift){
+    if()
+    let shiftedUnicode = unicode + shift;
+};
 // Do not edit below this line
 module.exports = caesar;
