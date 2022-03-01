437c2bc3a5cec70f71c267e30b2a1cf67cece04e
Bob <bobdt84@gmail.com>
2022-02-28 18:37:57 -0600

    Completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,13 +1,13 @@
 const fibonacci = function(n) {
-    if(n==0){
-        return 0;
-    } else if(n==1){
-        return 1;
+    if(n<0){
+        return "OOPS";
     }
-    let fib = 1;
-    for(let i= 0; i<n; i++){
-        fib = fib + 
+    let fibArray = [0,1];
+
+    for(let i= 2; i<=n; i++){
+        fibArray[i]  = fibArray[i-1] + fibArray[i-2]; 
     }
+    return fibArray[n];
 };
 
 // Do not edit below this line
