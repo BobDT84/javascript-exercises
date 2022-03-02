6c132e0e3058e8aced943961093679d65a4a17dd
Bob <bobdt84@gmail.com>
2022-03-01 18:54:31 -0600

    Consolidated thisYear variable into one line.  Removed unused index variable.  Extracted out to condition that checks if someone is still living and made a function to improve readability.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,18 +1,19 @@
 const findTheOldest = function(people) {
     let ages = [];
-    let thisYear = new Date();
-    thisYear = thisYear.getFullYear();
+    let thisYear = new Date().getFullYear();
     for(let i=0; i<people.length; i++){
-        if(isNaN(people[i].yearOfDeath)){
+        if(isStillAlive(people[i])){ 
             ages[i] = thisYear-people[i].yearOfBirth
         } else {
             ages[i] = people[i].yearOfDeath - people[i].yearOfBirth
         }
     }
-    let index = 0;
     indexOfOldest = ages.indexOf(Math.max(...ages));
     return people[indexOfOldest];
 };
 
+function isStillAlive(person){
+    return isNaN(person.yearOfDeath);
+}
 // Do not edit below this line
 module.exports = findTheOldest;
