5683c06c5f979acd0bec29ee3e7468f2b8c1f167
Bob <bobdt84@gmail.com>
2022-03-01 18:56:06 -0600

    added comments to help myself remember how it works at a future date

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -3,7 +3,10 @@ const fibonacci = function(n) {
         return "OOPS";
     }
     let fibArray = [0,1];
-
+    //Fibonacci 0 1 1 2 3 5 8 13 21 ...
+    //If array is currently at [0,1,1,2,3] and n is 5
+    //fibA[5] = fibA[4] + fibA[3]
+    //   5    =    3    +    2
     for(let i= 2; i<=n; i++){
         fibArray[i]  = fibArray[i-1] + fibArray[i-2]; 
     }
